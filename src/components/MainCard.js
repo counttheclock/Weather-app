4dfde884120047d0d359b415c14da5391a10de91
Jon Christensen <jonchristensen11@gmail.com>
2019-06-15 18:57:41 -0500

    changed https back to http

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -126,7 +126,7 @@ class MainCard extends Component {
     setTimeout(() => {
       console.log(this.state.lat);
       console.log(this.state.long);
-      fetch(`https://api.geonames.org/timezoneJSON?lat=${this.state.lat}&lng=${this.state.long}&username=${productionkeys.username}`)
+      fetch(`http://api.geonames.org/timezoneJSON?lat=${this.state.lat}&lng=${this.state.long}&username=${productionkeys.username}`)
         .then(res => res.json())
         .then(data =>
           this.setState({
